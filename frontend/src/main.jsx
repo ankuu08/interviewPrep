addf6a0525853c727de2105abc1a4300e4d62433
ankuu08 <127964677+ankuu08@users.noreply.github.com>
2026-04-04 23:10:44 +0530

    Update ChatProvider import in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,8 @@ import { Provider } from "@/components/ui/provider"
 import App from "./App.jsx";
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from "@/components/ui/toaster";
-import ChatProvider from "./context/chatProvider.jsx";
+// import ChatProvider from "./context/chatProvider.jsx";
+import { ChatProvider } from "./context/ChatProvider";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
