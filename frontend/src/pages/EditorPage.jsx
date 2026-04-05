2a821e2aa99e560e640cee31d3585ba5f36a79c7
ankuu08 <127964677+ankuu08@users.noreply.github.com>
2026-04-05 15:06:20 +0530

    Refactor socket connection and variable handling

--- frontend/src/pages/EditorPage.jsx
+++ frontend/src/pages/EditorPage.jsx
@@ -4,17 +4,17 @@ import { Menu, Portal } from "@chakra-ui/react"
 import Editor from '@monaco-editor/react';
 import { io } from 'socket.io-client';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom';
-const ENDPOINT = window.location.origin;
+// const ENDPOINT = "http://localhost:5000";
+const ENDPOINT=window.location.origin;
 var socket;
 function EditorPage() {
   const history = useHistory()
   const editorRef = useRef(null);
-  const typingTimeout = useRef(null);
   const hasjoined = useRef(false);
   const [code, setcode] = useState("");
   const [users, setusers] = useState([]);
-  const roomId=useRef();
-  const name=useRef();
+  var roomId;
+  var name;
   const lang = ['java', 'python', 'cpp', 'javascript']
   const [language, setlanguage] = useState("java");
   function handleEditorDidMount(editor, monaco) {
@@ -22,15 +22,15 @@ function EditorPage() {
     editor.focus();
   }
   useEffect(() => {
-    roomId.current = localStorage.getItem("code");
-    name.current = localStorage.getItem("name");
-    if (!roomId.current || !name.current) {
+    roomId = localStorage.getItem("code");
+    name = localStorage.getItem("name");
+    if (!roomId || !name) {
       history.push("/chats/editor")
     }
     socket = io(ENDPOINT,{
       transports:["websocket"]
     });
-    socket.emit("join-room", { roomId:roomId.current, name:name.current });
+    socket.emit("join-room", { roomId, name });
     socket.on("code-update", (newCode) => setcode(newCode));
     socket.on("user-update", (userList) => {
       setusers(userList);
@@ -43,21 +43,8 @@ function EditorPage() {
     })
   }, [])
   const handleChange = (value) => {
-    // setcode(value);
-    // socket.emit("code-change", { roomId:roomId.current, code: value })
     setcode(value);
-
-  if (typingTimeout.current) {
-    clearTimeout(typingTimeout.current);
-  }
-
-  typingTimeout.current = setTimeout(() => {
-    socket.emit("code-change", {
-      roomId: roomId.current,
-      code: value
-    });
-  }, 120); // 100–200ms is best
-};
+    socket.emit("code-change", { roomId, code: value })
   }
   return (
     <>
@@ -75,7 +62,7 @@ function EditorPage() {
                   {lang.map((l) => {
                     return <Menu.Item value={l} onClick={() => {
                       setlanguage(l)
-                      socket.emit("lang-change", { roomId:roomId.current, lang: l })
+                      socket.emit("lang-change", { roomId, lang: l })
                     }}>{l}</Menu.Item>
                   })}
 
